bf9cfd82d7a2580db23c2de5c75e69ba40eee438
Vijay Raghav Varada <vjvarada@users.noreply.github.com>
2024-07-20 21:59:04 +0530

    Update activeExtruder.py

--- octoprint_TwinDragon600x600CM4TouchUI/MainUIClass/MainUIClasses/activeExtruder.py
+++ octoprint_TwinDragon600x600CM4TouchUI/MainUIClass/MainUIClasses/activeExtruder.py
@@ -4,7 +4,7 @@ from PyQt5 import QtGui
 from MainUIClass.config import _fromUtf8
 
 class activeExtruder(mainGUI.Ui_MainWindow):
-    def init(self):
+    def __init__(self):
         super().__init__()
 
     def setup(self):
@@ -14,62 +14,74 @@ class activeExtruder(mainGUI.Ui_MainWindow):
         '''
         Selects the tool whose temperature needs to be changed. It accordingly changes the button text. it also updates the status of the other toggle buttons
         '''
-
-        if self.toolToggleChangeFilamentButton.isChecked():
-            self.setActiveExtruder(1)
-            self.octopiclient.selectTool(1)
-            time.sleep(1)
-
-        else:
-            self.setActiveExtruder(0)
-            self.bedLevelPrintButtonoctopiclient.selectTool(0)
-            time.sleep(1)
-
+        try:
+            if self.toolToggleChangeFilamentButton.isChecked():
+                self.setActiveExtruder(1)
+                self.octopiclient.selectTool(1)
+                time.sleep(1)
+            else:
+                self.setActiveExtruder(0)
+                self.octopiclient.selectTool(0)
+                time.sleep(1)
+        except Exception as e:
+            error_message = f"Error in selectToolChangeFilament: {str(e)}"
+            log_error(error_message)
+            if dialog.WarningOk(self, error_message, overlay=True):
+                pass
 
     def selectToolMotion(self):
         '''
         Selects the tool whose temperature needs to be changed. It accordingly changes the button text. it also updates the status of the other toggle buttons
         '''
-
-        if self.toolToggleMotionButton.isChecked():
-            self.setActiveExtruder(1)
-            self.octopiclient.selectTool(1)
-
-        else:
-            self.setActiveExtruder(0)
-            self.octopiclient.selectTool(0)
+        try:
+            if self.toolToggleMotionButton.isChecked():
+                self.setActiveExtruder(1)
+                self.octopiclient.selectTool(1)
+            else:
+                self.setActiveExtruder(0)
+                self.octopiclient.selectTool(0)
+        except Exception as e:
+            error_message = f"Error in selectToolMotion: {str(e)}"
+            log_error(error_message)
+            if dialog.WarningOk(self, error_message, overlay=True):
+                pass
 
     def selectToolTemperature(self):
         '''
         Selects the tool whose temperature needs to be changed. It accordingly changes the button text.it also updates the status of the other toggle buttons
         '''
-        # self.toolToggleTemperatureButton.setText(
-        #     "1") if self.toolToggleTemperatureButton.isChecked() else self.toolToggleTemperatureButton.setText("0")
-        if self.toolToggleTemperatureButton.isChecked():
-            print ("extruder 1 Temperature")
-            self.toolTempSpinBox.setProperty("value", float(self.tool1TargetTemperature.text()))
-        else:
-            print ("extruder 0 Temperature")
-            self.toolTempSpinBox.setProperty("value", float(self.tool0TargetTemperature.text()))
+        try:
+            if self.toolToggleTemperatureButton.isChecked():
+                print("extruder 1 Temperature")
+                self.toolTempSpinBox.setProperty("value", float(self.tool1TargetTemperature.text()))
+            else:
+                print("extruder 0 Temperature")
+                self.toolTempSpinBox.setProperty("value", float(self.tool0TargetTemperature.text()))
+        except Exception as e:
+            error_message = f"Error in selectToolTemperature: {str(e)}"
+            log_error(error_message)
+            if dialog.WarningOk(self, error_message, overlay=True):
+                pass
 
     def setActiveExtruder(self, activeNozzle):
-        activeNozzle = int(activeNozzle)
-        if activeNozzle == 0:
-            self.tool0Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/activeNozzle.png")))
-            self.tool1Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/Nozzle.png")))
-            self.toolToggleChangeFilamentButton.setChecked(False)
-            # self.toolToggleChangeFilamentButton.setText("0")
-            self.toolToggleMotionButton.setChecked(False)
-            self.toolToggleMotionButton.setText("0")
-            self.activeExtruder = 0
-        elif activeNozzle == 1:
-            self.tool0Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/Nozzle.png")))
-            self.tool1Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/activeNozzle.png")))
-            self.toolToggleChangeFilamentButton.setChecked(True)
-            # self.toolToggleChangeFilamentButton.setText("1")
-            self.toolToggleMotionButton.setChecked(True)
-            self.toolToggleMotionButton.setText("1")
-            self.activeExtruder = 1
-
-            # set button states
-            # set octoprint if mismatch
+        try:
+            activeNozzle = int(activeNozzle)
+            if activeNozzle == 0:
+                self.tool0Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/activeNozzle.png")))
+                self.tool1Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/Nozzle.png")))
+                self.toolToggleChangeFilamentButton.setChecked(False)
+                self.toolToggleMotionButton.setChecked(False)
+                self.toolToggleMotionButton.setText("0")
+                self.activeExtruder = 0
+            elif activeNozzle == 1:
+                self.tool0Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/Nozzle.png")))
+                self.tool1Label.setPixmap(QtGui.QPixmap(_fromUtf8("templates/img/activeNozzle.png")))
+                self.toolToggleChangeFilamentButton.setChecked(True)
+                self.toolToggleMotionButton.setChecked(True)
+                self.toolToggleMotionButton.setText("1")
+                self.activeExtruder = 1
+        except Exception as e:
+            error_message = f"Error in setActiveExtruder: {str(e)}"
+            log_error(error_message)
+            if dialog.WarningOk(self, error_message, overlay=True):
+                pass
